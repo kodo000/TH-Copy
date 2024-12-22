b8e45ed7a9ba707b3f384cedf80d4cdc06e54e00
kodo000 <github.legible672@passinbox.com>
2024-12-23 06:32:33 +0900

    v1.0.3 logic improved according powershell Copy command behavior, new banner

--- main.bat
+++ main.bat
@@ -1,7 +1,4 @@
 @echo off
-REM set "Params=%*"
-REM cd /d "%~dp0" && ( if exist "%Temp%\getadmin.vbs" del "%Temp%\getadmin.vbs") && fsutil dirty query %SystemDrive% 1>nul 2>nul || ( echo set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %Params%", "", "runas", 1 >> "%Temp%\getadmin.vbs" && "%Temp%\getadmin.vbs" && Exit /b)
-REM %WinDir%\System32\reg.exe query "HKU\S-1-5-19" 1>nul 2>nul || ( echo. & echo  ERROR: This Batch file MUST be run in an ELEVATED cmd prompt [ Administrator ] & echo. & echo         Right-Click the Batch file and click ^<Run as Administrator^>. & echo. & echo ^>Press ANY key to EXIT . . . & pause >nul & Exit )
 
 title Copy by Technical Home
 
@@ -20,6 +17,7 @@ echo             [38;2;51;204;255m██║     ██║   ██║██╔�
 echo             [38;2;0;204;255m╚██████╗╚██████╔╝██║        ██║[0m
 echo              [38;2;0;255;204m╚═════╝ ╚═════╝ ╚═╝        ╚═╝[0m
 echo.
+echo                  Technical Home — 2024    
 echo.
 
 exit /b
@@ -34,10 +32,17 @@ goto :exit
 :origin
 set /p origin="Origen: [38;2;147;151;155m"
 REM set /p origin=Origen: [38;2;57;255;20m
+
+if not exist "%origin%" (
+	echo [38;2;255;0;0mLa ruta de origen solicitada no existe. Presione enter para reintentar[0m
+	pause > nul
+	goto :origin
+)
+
 exit /b
 
 :destination
-set /p destination="Destino: [38;2;147;151;155m"
+set /p destination="[0mDestino: [38;2;147;151;155m"[0m
 REM set /p destination=Destino: [38;2;57;255;20m
 exit /b
 
